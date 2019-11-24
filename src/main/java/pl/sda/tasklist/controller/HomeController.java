e74cc896d997ed8f484fdb772a123b7d29b819c6
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2019-11-24 10:40:05 +0100

    Add HomeController

---
+++ src/main/java/pl/sda/tasklist/controller/HomeController.java
@@ -0,0 +1,30 @@
+package pl.sda.tasklist.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.tasklist.service.TaskCategoryService;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping
+public class HomeController {
+
+    private final TaskCategoryService taskCategoryService;
+
+    @GetMapping("/")
+    ModelAndView getIndex(Principal principal) {
+        ModelAndView modelAndView;
+        if (principal == null) {
+            modelAndView = new ModelAndView("index-anonymous");
+        } else {
+            modelAndView = new ModelAndView("index-logged-in");
+            modelAndView.addObject("taskCategories", taskCategoryService.getAllTaskCategoriesByUser(principal.getName()));
+        }
+        return modelAndView;
+    }
+}

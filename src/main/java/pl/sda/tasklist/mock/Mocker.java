cf8997a9ce7ad7078926c5abd877a5c026ac8ba4
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2019-11-24 21:35:11 +0100

    Populate the database with default roles during startup

--- src/main/java/pl/sda/tasklist/mock/Mocker.java
+++ src/main/java/pl/sda/tasklist/mock/Mocker.java
@@ -2,6 +2,8 @@ package pl.sda.tasklist.mock;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.sda.tasklist.dao.UserRoleRepository;
+import pl.sda.tasklist.model.UserRoleEntity;
 
 import javax.annotation.PostConstruct;
 
@@ -9,4 +11,14 @@ import javax.annotation.PostConstruct;
 @Service
 public class Mocker {
 
+    private final UserRoleRepository userRoleRepository;
+
+    @PostConstruct
+    public void populateRoles() {
+        UserRoleEntity ordinaryUserRole = new UserRoleEntity("ROLE_USER");
+        userRoleRepository.save(ordinaryUserRole);
+
+        UserRoleEntity adminUserRole = new UserRoleEntity("ROLE_ADMIN");
+        userRoleRepository.save(adminUserRole);
+    }
 }

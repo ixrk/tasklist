9be9b4bc3d422c93309c5c91e0d15b81a6d8cd93
Julia Smutek <juliasmutek.95@gmail.com>
2019-12-07 12:37:52 +0100

    add model and dao of user and user role EDIT lombok

--- src/main/java/pl/sda/tasklist/controller/AdminController.java
+++ src/main/java/pl/sda/tasklist/controller/AdminController.java
@@ -1,10 +1,13 @@
 package pl.sda.tasklist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.sda.tasklist.dao.UserRepository;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
     private final UserRepository userRepository;
 
@@ -12,6 +15,7 @@ public class AdminController {
         this.userRepository = userRepository;
     }
 
+    @GetMapping("/user-list")
     ModelAndView getAllUsers(){
         ModelAndView mnv = new ModelAndView("user-list");
         mnv.addObject("userList",userRepository.findAll());
